780b7de679189257aa61717b39bd074b4fb4c02f
Armin Radoncic <Arminradoncic1@gmail.com>
2023-03-24 14:10:52 -0400

    Add functions 'createGridRow' & 'createGridSquare'
    This commit adds the functions 'createGridRow' & 'createGridSquare' to
    main.js, and calls them in the 'createGrid' function. The change ensures
    the 'createGrid' function easier to read by following the Single
    Responsability Principle.
    
    The 'createGrid' function is now only responsible for bringing the
    different parts of the grid together (thus creating the grid), while the
    'createGridRow' and 'createGridSquare' functions are now responsible
    for creating the individual parts themselves.

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 function createGrid() {
     const gridContainer = document.getElementById("grid-container");
     for (let i = 0; i < 16; i++) {
-        const newGridRow = document.createElement("div");
-        newGridRow.classList.add("grid-row");
+        const newGridRow = createGridRow();
         gridContainer.appendChild(newGridRow);
         for (let i = 0; i < 16; i++) {
-            const newGridSquare = document.createElement("div");
-            newGridSquare.classList.add("grid-square");
+            const newGridSquare = createGridSquare();
             newGridRow.appendChild(newGridSquare);
         }
     }
+}
+
+function createGridRow() {
+    const newGridRow = document.createElement("div");
+    newGridRow.classList.add("grid-row");
+    return newGridRow;
+}
+
+function createGridSquare() {
+    const newGridSquare = document.createElement("div");
+    newGridSquare.classList.add("grid-square");
+    return newGridSquare;
 }
 
 createGrid();
