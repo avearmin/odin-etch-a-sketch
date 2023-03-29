d89f556aa43c04422de9a15117911048af5ae0f9
Armin Radoncic <Arminradoncic1@gmail.com>
2023-03-29 16:13:43 -0400

    Create logic for changing grid dimensions via slider

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-function createGrid() {
+function createGrid(sliderValue) {
     const gridContainer = document.getElementById("grid-container");
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < sliderValue; i++) {
         const newGridRow = createGridRow();
         gridContainer.appendChild(newGridRow);
-        for (let i = 0; i < 16; i++) {
+        for (let i = 0; i < sliderValue; i++) {
             const newGridSquare = createGridSquare();
             newGridRow.appendChild(newGridSquare);
         }
@@ -25,11 +25,44 @@ function createGridSquare() {
 function drawOnHover() {
     const gridSquares = document.querySelectorAll(".grid-square");
     gridSquares.forEach(gridSquare => {
-        gridSquare.addEventListener("mouseover", (event) => {
+        gridSquare.addEventListener("mouseover", () => {
                 gridSquare.style.backgroundColor = "black";
         });
     });
 }
 
-createGrid();
+function getSliderValue() {
+    const slider = document.getElementById("slider");
+    return slider.value;
+}
+
+function displaySliderValue() {
+    const slider = document.getElementById("slider");
+    const sliderValue = document.getElementById("slider-value");
+    slider.addEventListener("input", () => {
+        sliderValue.textContent = getSliderValue();
+    });
+}
+
+function changeGridDimensions() {
+    const gridContainer = document.getElementById("grid-container");
+    const slider = document.getElementById("slider");
+    slider.addEventListener("input", () => {
+        removeChildNodes(gridContainer);
+        createGrid(getSliderValue());
+        drawOnHover();
+    });
+}
+
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+createGrid(getSliderValue());
+displaySliderValue();
+changeGridDimensions();
 drawOnHover();
+
+
